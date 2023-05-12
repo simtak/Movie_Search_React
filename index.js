7341df24d03daa54be6eef375697ae938124bd4d
simon <skadel@posteo.de>
2023-05-12 17:16:16 +0200

    uploading code

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchMovies from "./searchMovies";
 
-ReactDOM.render(<App />, document.getElementById("root"))
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+        <SearchMovies/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
